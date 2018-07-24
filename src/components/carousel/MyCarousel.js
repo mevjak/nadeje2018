816152fc8921c4ec184417bfdfdbe2a00704e301
Miroslav Evjak <evjak@eset.cz>
2018-07-24 19:24:02 +0200

    Fix wrong text on button

--- src/components/carousel/MyCarousel.js
+++ src/components/carousel/MyCarousel.js
@@ -26,8 +26,8 @@ class MyCarousel extends React.Component {
                         <h1>Memoriál Petra Škrabálka</h1>
                         <p className="subtitle">Tradiční závod v přespolním běhu, který se pořádá již <strong>od
                             roku 1973</strong>.</p>
-                        <p><Link className="button" to={'/akce/memorial-petra-skrabalka'} role="button">Pojďte s
-                            námi proti rakovině!</Link></p>
+                        <p><Link className="button" to={'/akce/memorial-petra-skrabalka'} role="button">Ukaž, že jsi
+                            Mistr sportu!</Link></p>
                     </Carousel.Caption>
                 </Carousel.Item>
             </Carousel>

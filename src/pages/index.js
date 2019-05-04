bbb9c7cb70a14c0c7d6e218d98415af9667696c7
Miroslav Evjak <evjak@eset.cz>
2019-05-04 17:22:19 +0200

    Update event date for 2019, update sponsors list

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,98 +1,111 @@
 import React from 'react'
 import Link from 'gatsby-link'
 import Helmet from 'react-helmet'
-import {Table} from 'react-bootstrap'
+import { Table } from 'react-bootstrap'
 import { v4 } from 'uuid'
-import NumberFormat from 'react-number-format';
+import NumberFormat from 'react-number-format'
 
 import Article from '../components/article/Article'
 import MyCarousel from '../components/carousel/MyCarousel'
 
 class IndexPage extends React.Component {
-    render() {
-        const siteTitle = this.props.data.site.siteMetadata.title;
-        const posts = this.props.data.allMarkdownRemark.edges;
-        const history = this.props.data.allHistoryJson.edges;
+  render() {
+    const siteTitle = this.props.data.site.siteMetadata.title
+    const posts = this.props.data.allMarkdownRemark.edges
+    const history = this.props.data.allHistoryJson.edges
 
-        return (
-            <div>
-                <Helmet title={siteTitle}/>
+    return (
+      <div>
+        <Helmet title={siteTitle} />
 
-                <blockquote className="motto">
-                    <p>Kdo nedělá nic pro druhé, nedělá nic pro sebe.</p>
-                    <footer>Johann Wolfgang von Goethe</footer>
-                </blockquote>
+        <blockquote className="motto">
+          <p>Kdo nedělá nic pro druhé, nedělá nic pro sebe.</p>
+          <footer>Johann Wolfgang von Goethe</footer>
+        </blockquote>
 
-                <MyCarousel/>
+        <MyCarousel />
 
-                <div className="row">
-                    <div className="col-md-9">
-                        <h3>Aktuální články</h3>
+        <div className="row">
+          <div className="col-md-9">
+            <h3>Aktuální články</h3>
 
-                        {posts.map(({node}) => {
-                            return (
-                                <Article key={node.fields.slug} article={node}/>
-                            )
-                        })}
+            {posts.map(({ node }) => {
+              return <Article key={node.fields.slug} article={node} />
+            })}
+          </div>
 
-                    </div>
-
-                    <div className="col-md-3">
-
-                        {/*<div className="banner">*/}
-                            {/*<h4><span className="glyphicon glyphicon-calendar" aria-hidden="true"/>&nbsp; Běh naděje 2018</h4>*/}
-                            {/*<p className="lead">8. září, 10:00</p>*/}
-                        {/*</div>*/}
-
-                        <div className="banner">
-                            <h4><span className="glyphicon glyphicon-piggy-bank" aria-hidden="true"/>&nbsp; Výtěžek sbírky 2018</h4>
-                            <p className="lead">79 788,-</p>
-                        </div>
-
-                        <h4 className="banner-heading">Historie běhů v Jiřetíně p.B.</h4>
-                        <div className="well">
-                            <Table condensed striped>
-                                <thead>
-                                <tr>
-                                    <th>Rok</th>
-                                    <th>Účastníků</th>
-                                    <th style={{textAlign: 'right'}}>Výtěžek</th>
-                                </tr>
-                                </thead>
-                                <tbody>
-                                {history.map(({node}) => {
-                                    return (
-                                        <tr key={v4()}>
-                                            <td>{node.eventYear}</td>
-                                            <td style={{textAlign: 'center'}}>{node.participantCount}</td>
-                                            <td style={{textAlign: 'right'}}>
-                                                <NumberFormat value={node.moneyRaised} displayType={'text'} thousandSeparator={' '} suffix={',- Kč'} />
-                                            </td>
-                                        </tr>
-                                    )
-                                })}
-                                </tbody>
-                            </Table>
-                        </div>
-
-                    </div>
-                </div>
-
-                <div className="row">
-                    <div className="col-md-8">
-                        <nav className="articles-nav">
-                            <ul className="pager">
-                                <li className="previous">
-                                    <Link to={'/clanky/'}>Všechny články <span aria-hidden="true">&rarr;</span></Link>
-                                </li>
-                            </ul>
-                        </nav>
-                    </div>
-                </div>
-
+          <div className="col-md-3">
+            <div className="banner">
+              <h4>
+                <span
+                  className="glyphicon glyphicon-calendar"
+                  aria-hidden="true"
+                />&nbsp; Běh naděje 2019
+              </h4>
+              <p className="lead">14. září, 10:00</p>
             </div>
-        )
-    }
+
+            <div className="banner">
+              <h4>
+                <span
+                  className="glyphicon glyphicon-piggy-bank"
+                  aria-hidden="true"
+                />&nbsp; Výtěžek sbírky 2018
+              </h4>
+              <p className="lead">79 788,-</p>
+            </div>
+
+            <h4 className="banner-heading">Historie běhů v Jiřetíně p.B.</h4>
+            <div className="well">
+              <Table condensed striped>
+                <thead>
+                  <tr>
+                    <th>Rok</th>
+                    <th>Účastníků</th>
+                    <th style={{ textAlign: 'right' }}>Výtěžek</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {history.map(({ node }) => {
+                    return (
+                      <tr key={v4()}>
+                        <td>{node.eventYear}</td>
+                        <td style={{ textAlign: 'center' }}>
+                          {node.participantCount}
+                        </td>
+                        <td style={{ textAlign: 'right' }}>
+                          <NumberFormat
+                            value={node.moneyRaised}
+                            displayType={'text'}
+                            thousandSeparator={' '}
+                            suffix={',- Kč'}
+                          />
+                        </td>
+                      </tr>
+                    )
+                  })}
+                </tbody>
+              </Table>
+            </div>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-md-8">
+            <nav className="articles-nav">
+              <ul className="pager">
+                <li className="previous">
+                  <Link to={'/clanky/'}>
+                    Všechny články <span aria-hidden="true">&rarr;</span>
+                  </Link>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default IndexPage
@@ -105,8 +118,8 @@ export const pageQuery = graphql`
       }
     }
     allMarkdownRemark(
-        limit: 5
-        sort: { fields: [frontmatter___date], order: DESC }
+      limit: 5
+      sort: { fields: [frontmatter___date], order: DESC }
     ) {
       edges {
         node {
@@ -120,11 +133,11 @@ export const pageQuery = graphql`
             author
             description
             featuredImage {
-                childImageSharp{
-                    sizes(maxWidth: 900, maxHeight: 200, cropFocus: CENTER) {
-                        ...GatsbyImageSharpSizes
-                    }
+              childImageSharp {
+                sizes(maxWidth: 900, maxHeight: 200, cropFocus: CENTER) {
+                  ...GatsbyImageSharpSizes
                 }
+              }
             }
           }
         }
@@ -136,8 +149,8 @@ export const pageQuery = graphql`
           eventYear
           participantCount
           moneyRaised
-        } 
+        }
       }
     }
   }
-`;
+`

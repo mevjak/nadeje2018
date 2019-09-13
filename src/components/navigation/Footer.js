1c1b906b7403951f4d50f1727c5a5c2b042d77d4
Miroslav Evjak <evjak@eset.cz>
2019-09-13 14:57:25 +0200

    Fix broken social links in footer

--- src/components/navigation/Footer.js
+++ src/components/navigation/Footer.js
@@ -11,12 +11,12 @@ class Footer extends React.Component {
                     <Links dir={'left'}/>
                 </ul>
                 <ul className="nav navbar-nav navbar-right">
-                    <li><Link to={'https://www.facebook.com/nadeje2011'} className="social social-fb"
-                           target="_blank">FB</Link></li>
-                    <li><Link to={'https://twitter.com/nadeje_2011'} className="social social-tw"
-                           target="_blank">TW</Link></li>
-                    <li><Link to={'https://plus.google.com/+Nadeje2011CzTeam/about'} className="social social-gg"
-                           target="_blank">G+</Link></li>
+                    <li>
+                        <a href="https://www.facebook.com/nadeje2011" className="social social-fb" target="_blank">FB</a>
+                    </li>
+                    <li>
+                        <a href="https://twitter.com/nadeje_2011" className="social social-tw" target="_blank">TW</a>
+                    </li>
                 </ul>
                 <div className="clearfix" />
             </footer>
